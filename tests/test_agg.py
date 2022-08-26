71aa36e6782586bd728e11899bf45851036a61b6
Aleksander Durumeric <alekepd@gmail.com>
2022-08-26 13:20:29 -0500

    Neatened test_agg
    Formatted with black, removed unused assignment.

--- tests/test_agg.py
+++ tests/test_agg.py
@@ -14,16 +14,19 @@ def test_agg_opt():
     dimerfile = os.path.join(os.path.dirname(__file__), "data/waterdimer.npz")
     dimerdata = np.load(dimerfile)
     forces = dimerdata["Fs"]
-    coords = dimerdata["coords"]
 
     # CG mapping: two oxygens
     inds = [[0], [3]]
     cmap = lm.LinearMap(inds, n_fg_sites=forces.shape[1])
     optim_results = ag.project_forces(
-        xyz=None, forces=forces, config_mapping=cmap, constrained_inds=set(), solver_args={"solver": "scs"}
+        xyz=None,
+        forces=forces,
+        config_mapping=cmap,
+        constrained_inds=set(),
+        solver_args={"solver": "scs"},
     )
 
     # aggregation mapping: we expect that contributions from each water are added up
     # to cancel the intramolecular bond forces
     agg_mapping = np.array([[1, 1, 1, 0, 0, 0], [0, 0, 0, 1, 1, 1]], dtype=float)
-    assert np.allclose(optim_results['map']._standard_matrix, agg_mapping, atol=5e-3)
+    assert np.allclose(optim_results["map"]._standard_matrix, agg_mapping, atol=5e-3)
