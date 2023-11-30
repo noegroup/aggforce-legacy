59f5d232ba893766b8adceed9751a05f73e736f0
Aleksander Durumeric <alekepd@gmail.com>
2023-11-30 09:31:32 -0600

    Ran black.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Installation stub."""
 
-import setuptools # type: ignore [import-untyped]
+import setuptools  # type: ignore [import-untyped]
 
 if __name__ == "__main__":
     setuptools.setup()
